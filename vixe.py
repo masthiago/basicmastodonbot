5f6f11138c4895f83326b3f896e050096f515dcd
Thiago Medeiros <thiago@tuta.io>
2022-10-12 10:35:54 -0300

    file and toot with next str and time

--- vixe.py
+++ vixe.py
@@ -1,4 +1,4 @@
-#!./venv/bin/python3
+from datetime import datetime, timedelta
 from pathlib import Path
 from random import randint
 from time import sleep
@@ -38,7 +38,7 @@ try:
         )
 
 except Exception as err:
-    print('Authentication failed. {}'.format(err))
+    print('\nAuthentication failed. \n{}\n'.format(err))
     exit(1)
 
 
@@ -54,14 +54,28 @@ try:
             'xis': 'x' * randint(1, 8),
             'es': 'e' * randint(1, 8),
         }
-        vixe = 'v{is}{xis}{es}!'.format(**repeat)
+
+        vixe = 'V{is}{xis}{es}!'.format(**repeat)
+
+        next = (datetime.now() + timedelta(seconds=interval)).strftime(
+            '%d/%m/%Y %H:%Mh'
+        )
+
+        next_str = 'Next: \n\n{} \n\n{}'.format(next, vixe)
+
+        with open('next.txt', 'w') as next_file:
+            next_file.write(next_str + '\n')
+
+        cuscuzin.status_post(next_str, visibility='direct')
+
         sleep(interval)
         cuscuzin.toot(vixe)
 
+
 except KeyboardInterrupt:
-    print('Exit')
+    print('\nExit\n')
     exit(0)
 
 except Exception as err:
-    print('Repeat failure. {}'.format(err))
+    print('\nRepeat failure. \n{}\n'.format(err))
     exit(2)
